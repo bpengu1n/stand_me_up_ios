2cfd58fb8bb4e1d224f419e32ab6f0725fe2cdae
Benjamin Yunker <bwyunker@gmail.com>
2017-09-21 22:01:25 -0400

    Initial Commit

---
+++ StandMeUp_dev2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StandMeUp_dev2
+//
+//  Created by Benjamin Yunker on 9/21/17.
+//  Copyright © 2017 Benjamin Yunker. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
